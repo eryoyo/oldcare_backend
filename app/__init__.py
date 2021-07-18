59e798c9d37a7fc627a5a1af8fed91a12c297dbe
eryo12138 <15171271104@163.com>
2021-07-18 18:27:31 +0800

    初始化

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,17 @@ from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from config import config_map, redis_store
 from flask_mail import Mail
+from flask_socketio import SocketIO, emit
 
 import pymysql
 
+# 数据库
 db = SQLAlchemy()
+# 邮箱
 mail = Mail()
+# websocket
+async_mode = None
+socketio = SocketIO()
 
 
 def create_app(dev_name):
@@ -23,7 +29,9 @@ def create_app(dev_name):
 
     db.init_app(app)  # 实例化的数据库 配置信息
 
-    mail.init_app(app)  # 实例化的邮箱 配置信息
+    mail.init_app(app)  # 实例化的邮箱 配置信息, async_mode='eventlet'
+
+    socketio.init_app(app=app, cors_allowed_origins='*')
 
     # 利用flask-session，将session数据保存到redis中
     Session(app)
@@ -35,6 +43,7 @@ def create_app(dev_name):
     from app import sys_admin
     from app import volunteer
     from app import camera
+    from app import wechat
 
     app.register_blueprint(event.event, url_prefix="/event")  # 绑定包里面的蓝图对象
     app.register_blueprint(old_person.old_person, url_prefix="/old_person")
@@ -42,4 +51,6 @@ def create_app(dev_name):
     app.register_blueprint(sys_admin.sys_admin, url_prefix="/sys_admin")
     app.register_blueprint(volunteer.volunteer, url_prefix="/volunteer")
     app.register_blueprint(camera.camera, url_prefix="/camera")
+    app.register_blueprint(wechat.wechat, url_prefix="/")
+
     return app
